a62dd4a17a54933a592bb31165c9db6efc96aa83
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-01-17 23:14:06 +0530

    Changed DB Structure

--- app/src/main/java/com/keerthi77459/attendease/db/DbHelper.kt
+++ app/src/main/java/com/keerthi77459/attendease/db/DbHelper.kt
@@ -8,12 +8,8 @@ import com.keerthi77459.attendease.utils.Utils
 open class DbHelper(context: Context) : SQLiteOpenHelper(context, Utils().DB_NAME,null, Utils().DB_VERSION){
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE ${Utils().TABLE_STUDENT_DETAIL} (" +
-                "rollNo TEXT ,name TEXT,degree TEXT,class TEXT,year TEXT,phoneNumber TEXT,mode TEXT)")
         db?.execSQL("CREATE TABLE ${Utils().TABLE_CLASS_DETAIL}("+
                 "degree TEXT,class TEXT,year TEXT)")
-        db?.execSQL("CREATE TABLE ${Utils().TABLE_ATTENDANCE_DETAIL}(" +
-                "rollNo TEXT)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
